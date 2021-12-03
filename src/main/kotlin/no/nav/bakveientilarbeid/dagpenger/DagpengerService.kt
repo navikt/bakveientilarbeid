33b12c3f6fb4833bd9f6082f0aac07459661cca6
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-12-03 20:34:56 +0100

    f debuglogging

--- src/main/kotlin/no/nav/bakveientilarbeid/dagpenger/DagpengerService.kt
+++ src/main/kotlin/no/nav/bakveientilarbeid/dagpenger/DagpengerService.kt
@@ -9,8 +9,10 @@ class DagpengerService(
     private val dagpengerTokendings: DagpengerTokendings
 ) {
     suspend fun hentSoknad(user: AuthenticatedUser): Json {
-        logger.info("Henter dagpengesøknader for bruker")
+        logger.info("Henter tokenx for bruker")
         val token = dagpengerTokendings.exchangeToken(user)
+        logger.info("Fant tokenx for bruker $token")
+        logger.info("Henter dagpengesøknader for bruker")
         return dagpengerConsumer.hentSoknad(token)
     }
 }
