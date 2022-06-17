c60f1270477fca890d27412832941fbdc5a8abc9
Jostein Holje <jostein.holje@nav.no>
2022-06-17 12:23:25 +0200

    f Bedre logging på meldekort og pto-proxy

--- src/main/kotlin/no/nav/bakveientilarbeid/meldekort/meldekortRoute.kt
+++ src/main/kotlin/no/nav/bakveientilarbeid/meldekort/meldekortRoute.kt
@@ -5,14 +5,25 @@ import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import no.nav.bakveientilarbeid.auth.AuthenticatedUserService
+import no.nav.bakveientilarbeid.config.logger
 
 fun Route.meldekortRoute(meldekortService: MeldekortService, authenticatedUserService: AuthenticatedUserService) {
 
     get("/meldekort") {
-        call.respond(HttpStatusCode.OK, meldekortService.hentMeldekort(authenticatedUserService.getAuthenticatedUser(call)))
+        try {
+            call.respond(HttpStatusCode.OK, meldekortService.hentMeldekort(authenticatedUserService.getAuthenticatedUser(call)))
+        } catch (exception: Exception) {
+            logger.warn("Feil ved henting av meldekort", exception)
+            call.respond(HttpStatusCode.ServiceUnavailable)
+        }
     }
 
     get("/meldekort/status") {
-        call.respond(HttpStatusCode.OK, meldekortService.hentStatus(authenticatedUserService.getAuthenticatedUser(call)))
+        try {
+            call.respond(HttpStatusCode.OK, meldekortService.hentStatus(authenticatedUserService.getAuthenticatedUser(call)))
+        } catch (exception: Exception) {
+            logger.warn("Feil ved henting av meldekort-status", exception)
+            call.respond(HttpStatusCode.ServiceUnavailable)
+        }
     }
 }
--- src/main/kotlin/no/nav/bakveientilarbeid/ptoproxy/ptoProxyRoute.kt
+++ src/main/kotlin/no/nav/bakveientilarbeid/ptoproxy/ptoProxyRoute.kt
@@ -6,11 +6,12 @@ import io.ktor.client.features.*
 import io.ktor.client.statement.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import kotlinx.html.InputType
 import no.nav.bakveientilarbeid.auth.AccessToken
 import no.nav.bakveientilarbeid.auth.AuthenticatedUserService
+import no.nav.bakveientilarbeid.config.logger
 import no.nav.bakveientilarbeid.http.getWithConsumerId
 import no.nav.bakveientilarbeid.http.postWithConsumerId
+import org.slf4j.Logger
 import java.net.URL
 
 fun Route.ptoProxyRoute(
@@ -22,43 +23,43 @@ fun Route.ptoProxyRoute(
     get("/oppfolging") {
         val token = AccessToken(authenticatedUserService.getAuthenticatedUser(call).token)
         val oppfolgingUrl = URL("$PTO_PROXY_URL/veilarboppfolging/api/oppfolging")
-        handleRequest(call, httpClient, token, oppfolgingUrl)
+        handleRequest(call, httpClient, token, oppfolgingUrl, logger)
     }
 
     get("/underoppfolging") {
         val token = AccessToken(authenticatedUserService.getAuthenticatedUser(call).token)
         val underOppfolgingUrl = URL("$PTO_PROXY_URL/veilarboppfolging/api/niva3/underoppfolging")
-        handleRequest(call, httpClient, token, underOppfolgingUrl)
+        handleRequest(call, httpClient, token, underOppfolgingUrl, logger)
     }
 
     get("/startregistrering") {
         val token = AccessToken(authenticatedUserService.getAuthenticatedUser(call).token)
         val startRegistreringUrl = URL("$PTO_PROXY_URL/veilarbregistrering/api/startregistrering")
-        handleRequest(call, httpClient, token, startRegistreringUrl)
+        handleRequest(call, httpClient, token, startRegistreringUrl, logger)
     }
 
     get("/registrering") {
         val token = AccessToken(authenticatedUserService.getAuthenticatedUser(call).token)
         val registreringUrl = URL("$PTO_PROXY_URL/veilarbregistrering/api/registrering")
-        handleRequest(call, httpClient, token, registreringUrl)
+        handleRequest(call, httpClient, token, registreringUrl, logger)
     }
 
     get("/dialog/antallUleste") {
         val token = AccessToken(authenticatedUserService.getAuthenticatedUser(call).token)
         val dialogUrl = URL("$PTO_PROXY_URL/veilarbdialog/api/dialog/antallUleste")
-        handleRequest(call, httpClient, token, dialogUrl)
+        handleRequest(call, httpClient, token, dialogUrl, logger)
     }
 
     get("/vedtakinfo/besvarelse") {
         val token = AccessToken(authenticatedUserService.getAuthenticatedUser(call).token)
         val besvarelseUrl = URL("$PTO_PROXY_URL/veilarbvedtakinfo/api/behovsvurdering/besvarelse")
-        handleRequest(call, httpClient, token, besvarelseUrl)
+        handleRequest(call, httpClient, token, besvarelseUrl, logger)
     }
 
     get("/vedtakinfo/motestotte") {
         val token = AccessToken(authenticatedUserService.getAuthenticatedUser(call).token)
         val motestotteUrl = URL("$PTO_PROXY_URL/veilarbvedtakinfo/api/motestotte")
-        handleRequest(call, httpClient, token, motestotteUrl)
+        handleRequest(call, httpClient, token, motestotteUrl, logger)
     }
 
     get("/arbeidssoker/perioder") {
@@ -72,13 +73,14 @@ fun Route.ptoProxyRoute(
             },
             onFailure = {
                 val exception = it as ResponseException
+                logger.warn("Feil ved kall til pto-proxy=${perioderUrl} status=${exception.response.status}")
                 call.respondBytes(status = exception.response.status, bytes = exception.response.readBytes())
             }
         )
     }
 }
 
-suspend fun handleRequest(call: ApplicationCall, httpClient: HttpClient, token: AccessToken, url: URL) {
+suspend fun handleRequest(call: ApplicationCall, httpClient: HttpClient, token: AccessToken, url: URL, logger: Logger) {
     Result.runCatching {
         httpClient.getWithConsumerId<HttpResponse>(url, token)
     }.fold(
@@ -87,6 +89,7 @@ suspend fun handleRequest(call: ApplicationCall, httpClient: HttpClient, token: 
         },
         onFailure = {
             val exception = it as ResponseException
+            logger.warn("Feil ved kall til pto-proxy=${url} status=${exception.response.status}")
             call.respondBytes(status = exception.response.status, bytes = exception.response.readBytes())
         }
     )
